1d2554c08a93d8719ba295f4070f14df84dbed6f
mohammedSaad3030 <54086365+mohammedSaad3030@users.noreply.github.com>
2019-08-14 17:58:54 +0200

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -1,3 +1,6 @@
+
+const double PI = 3.14159265358979323846;
+
 ///gcd
 int gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b ); }
 
@@ -18,4 +21,73 @@ for(int i = 1 ; i <= g/i ; i++)
 ll ceilDiv(ll a, ll b)
 {
     return (a + b - 1)/b;
+}
+
+
+void dfs(int node)
+{
+	visited[node] = true;
+
+	rep(i, adj[node])
+	{
+		int child = adj[node][i];
+		if (!visited[child])	// To avoid cyclic behavior
+			dfs(child);
+	}
+
+	topsort.push_back(node);	// DAG // Other way Indegree / Outdegree
+}
+
+int ConnectedComponenetsCnt()
+{
+	int cnt = 0;
+	lp(i, n)
+	{
+		if(!visited[i])	// Then no one reach this isolated node yet and its neighbors.
+		{
+			dfs(i);
+			cnt++;
+		}
+	}
+	return cnt;
+}
+
+void dfs_EdgeClassification(int node)
+{
+	start[node] = timer++;
+
+	rep(i, adj[node])
+	{
+		int child = adj[node][i];
+		if (start[child] == -1)	// Not visited Before. Treed Edge
+			dfs_EdgeClassification(child);
+		else {
+			if(finish[child] == -1)// then this is ancestor that called us and waiting us to finish. Then Cycle. Back Edge
+				anyCycle = 1;
+			else if(start[node] < start[child])	// then you are my descendant
+				;	// Forward Edge
+			else
+				;	// Cross Edge
+		}
+	}
+
+	finish[node] = timer++;
+}
+
+
+int cnt = 0;
+void cntReachalbleCells(int r, int c)
+{
+    if( !valid(r, c) || maze[r][c] == 'X' || vis[r][c] == 1)
+		return;		// invalid position or block position
+
+
+	vis[r][c] = 1;	// we just visited it, don't allow any one back to it
+	cnt++;
+
+	// Try the 4 neighbor cells
+	cntReachalbleCells(r, c-1);
+    cntReachalbleCells(r, c+1);
+    cntReachalbleCells(r-1, c);
+    cntReachalbleCells(r+1, c);
 }
